71b798b03f1f330a7047b400749be14d337c4fa9
Dawit Belay <113282163+DawitSamson@users.noreply.github.com>
2022-11-27 11:26:38 +0200

    text change

--- add_extra.py
+++ add_extra.py
@@ -2,8 +2,10 @@ extra_cheese = 0
 extra = 2
 net_price = 0
 
+
 def add_extra_menu():
     global extra_cheese
+    from piizza_order import net_price
     add_extra = input("\nWould you like an extra cheese? yes/no: ")
     if add_extra == "yes" or add_extra == "y":
         qty_extra = int(input("How many extra cheese you need? "))
